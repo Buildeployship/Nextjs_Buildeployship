fa6730a078aa9792e3d0120988702cd46ea3cd31
Buildeployship <197860382+Buildeployship@users.noreply.github.com>
2025-08-28 19:52:11 -0700

    navbar background white

--- src/ui/Navbar.js
+++ src/ui/Navbar.js
@@ -6,7 +6,7 @@ import Link from 'next/link'
 export default function Navbar() {
   return (
     <div>
-        <nav className="container mx-auto flex justify-center items-center top-0 z-10 p-5 min-w-full fixed gap-4 flex-col sm:flex-row">
+        <nav className="container mx-auto flex justify-center items-center top-0 p-5 min-w-full fixed gap-4 flex-col sm:flex-row bg-white">
 
         {/* Home Link */}
           <Link href="/" className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
